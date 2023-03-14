fc413451c288e55ace84bb1344fb768dad1783ba
Helen Byelyayeva <91469540+helenbyelyayeva@users.noreply.github.com>
2023-03-14 22:35:48 +0200

    p2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,18 +7,11 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const galleryListIt = document.querySelector(".gallery");
 galleryListIt.setAttribute('uk-lightbox', 'caption-position:bottom');
-
-
 galleryListIt.addEventListener('click', ongalleryListItClick);
 
-
 function createGalleryItem(array) {
-
-
    return array.map(({ preview, original, description }) => {
        const item = document.createElement('a');
-
-
        item.href = original;
        item.classList.add('gallery__item');
        item.dataset.caption = description;
@@ -28,54 +21,27 @@ function createGalleryItem(array) {
        image.alt = description;
        image.title = description;
        item.append(image);
-
-
        return item;
    })
 
 
 }
 
-
-
-
 const galleryElements = createGalleryItem(galleryItems);
 galleryListIt.append(...galleryElements);
-
-
-
-
-
-
-
-
 function ongalleryListItClick(event) {
    event.preventDefault();
-
-
    if (event.target.nodeName !== "IMG") {
        return
    };
-
-
-
-
-
-
    let href = (event.target.closest('a').getAttribute('href'));
    return href;
 }
 let gallery = new SimpleLightbox('.gallery a', { caption: true, captionPosition: 'bottom', captionDelay: 250 });
 
 
-
-
 gallery.on('show.simplelightbox', function () {
 });
-
-
-
-
 console.log(galleryItems);
 
 
